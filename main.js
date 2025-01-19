b49142facd66c2bc56a19a95e98a1a3d4d3a2683
mavverie <128604315+ddandelioonn@users.noreply.github.com>
2025-01-19 15:09:27 +0500

    Update main.js

--- main.js
+++ main.js
@@ -1,9 +1,11 @@
-import * as THREE from 'three';
+import * as THREE from 'three'; 
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
 import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
+// Создание сцены
 const scene = new THREE.Scene();
 
+// Настройка рендерера
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.outputColorSpace = THREE.SRGBColorSpace;
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -13,9 +15,11 @@ renderer.shadowMap.enabled = true;
 renderer.shadowMap.type = THREE.PCFSoftShadowMap;
 document.body.appendChild(renderer.domElement);
 
+// Настройка камеры
 const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 1, 1000);
 camera.position.set(10, 5, 10);
 
+// Настройка управления камерой
 const controls = new OrbitControls(camera, renderer.domElement);
 controls.enableDamping = true;
 controls.enablePan = false;
@@ -27,16 +31,18 @@ controls.autoRotate = false;
 controls.target = new THREE.Vector3(0, 1, 0);
 controls.update();
 
+// Источник света
 const spotLight = new THREE.SpotLight(0xffffff, 3000, 100, 0.25, 1);
 spotLight.position.set(0, 25, 0);
 spotLight.castShadow = true;
 spotLight.shadow.bias = -0.0001;
 scene.add(spotLight);
 
+// Общий свет
 const ambientLight = new THREE.AmbientLight(0xffffff, 0.5); 
 scene.add(ambientLight);
 
-
+// Создание пола
 const groundGeometry = new THREE.PlaneGeometry(20, 20, 30, 30);
 groundGeometry.rotateX(-Math.PI / 2);
 const groundMaterial = new THREE.MeshStandardMaterial({
@@ -48,31 +54,33 @@ groundMesh.castShadow = false;
 groundMesh.receiveShadow = true;
 scene.add(groundMesh);
 
-let matildaModel = null;
+// Загрузка 3D модели LeePerrySmith.glb
+let model = null;
+const loader = new GLTFLoader().setPath('models/'); // Путь к папке с моделью
 
-const loader = new GLTFLoader().setPath('matilda/');
-loader.load('scene.gltf', (gltf) => {
-  console.log('loading model');
-  matildaModel = gltf.scene;
+loader.load('LeePerrySmith.glb', (gltf) => {
+  console.log('Model loaded');
+  model = gltf.scene;
 
-  matildaModel.traverse((child) => {
+  model.traverse((child) => {
     if (child.isMesh) {
       child.castShadow = true;
       child.receiveShadow = true;
     }
   });
 
-  matildaModel.position.set(0, 0, 0);
-  matildaModel.scale.set(1, 1, 1);
-  scene.add(matildaModel);
-
+  model.position.set(0, 0, 0);
+  model.scale.set(1, 1, 1);
+  scene.add(model);
 });
 
-
+// Анимация
 function animate() {
   requestAnimationFrame(animate);
-
-  matildaModel.rotation.y += 0.02;
+  
+  if (model) {
+    model.rotation.y += 0.02; // Поворот модели для анимации
+  }
 
   controls.update();
   renderer.render(scene, camera);
